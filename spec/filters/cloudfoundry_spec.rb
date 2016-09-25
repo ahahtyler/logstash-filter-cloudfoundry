42b5bb8169dcef5a595a671e37effb4e8bcdc783
ahahtyler <tyler.stigliano@verizon.com>
2016-09-24 23:56:31 -0400

    Update cloudfoundry_spec.rb

--- spec/filters/cloudfoundry_spec.rb
+++ spec/filters/cloudfoundry_spec.rb
@@ -1,26 +1,41 @@
-require 'logstash/devutils/rspec/spec_helper'
-require 'logstash/filters/cloudfoundry'
+# encoding: utf-8
+require "logstash/devutils/rspec/spec_helper"
+require "logstash/filters/cloudfoundry"
 
 describe LogStash::Filters::CloudFoundry do
 
-	describe "Retrieving meta-data" do
+  subject(:plugin) { LogStash::Filters::CloudFoundry.new(config) }
+  let(:config) { Hash.new }
 
-		config <<-CONFIG
-			filter {
-				cloudfoundry {
-					cf_api      => "https://api.run.pivotal.io"
-					cf_user     => "my-username"
-					cf_password => "my-password"
-					cf_org      => "ITCS"
-					cf_space    => "development"
-				}
-			}
-		CONFIG
-		
-		sample("message" => "abc loggregator 964db4e9-4f9c-42a2-9296-f8381ce459eb sample log") do
-			insist { subject.get("appguid") } == "ITCS"
-		end
+  let(:doc)   { "" }
+  let(:event) { LogStash::Event.new("message" => doc) }
 
-	end #end describe "retrieving meta-data"
+  describe "Receive" do
 
-end #end LogStash::Filters::CloudFoundry
+    before(:each) do
+      plugin.register
+    end
+
+    describe "Curl CF CLI" do
+
+      let(:config) { {"cf_api"      => "https://api.run.pivotal.io",
+                      "cf_user"     => "username",
+                      "cf_password" => "password",
+                      "cf_org"      => "ITCS",
+                      "cf_space"    => "development"} }
+
+      let(:doc) { "abc loggregator 964db4e9-4f9c-42a2-9296-f8381ce459eb sample log" }
+
+      it "extract all the values" do
+        plugin.filter(event)
+        expect(event.get("orgname")).to eq("ITCS")
+        expect(event.get("spacename")).to eq("development")
+	expect(event.get("appname")).to eq("spring-music")
+	expect(event.get("appguid")).to eq("964db4e9-4f9c-42a2-9296-f8381ce459eb")
+      end
+
+    end
+
+  end
+
+end
