b7e78bcc779ede814a3ac35000f8eabe17619dd2
ahahtyler <tyler.stigliano@verizon.com>
2016-09-24 22:21:14 -0400

    Update cloudfoundry_spec.rb

--- spec/filters/cloudfoundry_spec.rb
+++ spec/filters/cloudfoundry_spec.rb
@@ -1,20 +1,26 @@
-require 'spec_helper'
-require "logstash/filters/cloudfoundry"
+require 'logstash/devutils/rspec/spec_helper'
+require 'logstash/filters/cloudfoundry'
 
 describe LogStash::Filters::CloudFoundry do
 
-  describe "Set to Hello World" do
-    let(:config) do <<-CONFIG
-      filter {
-        cloudfoundry {
-        }
-      }
-    CONFIG
-    end
+	describe "Retrieving meta-data" do
 
-    sample("message" => "some text") do
-      expect(subject).to include("message")
-      expect(subject['message']).to eq('Hello World')
-    end
-  end
-end
+		config <<-CONFIG
+			filter {
+				cloudfoundry {
+					cf_api      => "https://api.run.pivotal.io"
+					cf_user     => "my-username"
+					cf_password => "my-password"
+					cf_org      => "ITCS"
+					cf_space    => "development"
+				}
+			}
+		CONFIG
+		
+		sample("message" => "abc loggregator 964db4e9-4f9c-42a2-9296-f8381ce459eb sample log") do
+			insist { subject.get("appguid") } == "ITCS"
+		end
+
+	end #end describe "retrieving meta-data"
+
+end #end LogStash::Filters::CloudFoundry
