12959f46eb3e70516a7945ced157c6188bde7fa5
ahahtyler <tyler.stigliano@verizon.com>
2016-09-26 16:40:19 -0400

    Update cloudfoundry_spec.rb

--- spec/filters/cloudfoundry_spec.rb
+++ spec/filters/cloudfoundry_spec.rb
@@ -1,20 +1,48 @@
-require 'spec_helper'
+# encoding: utf-8
+require "logstash/devutils/rspec/spec_helper"
 require "logstash/filters/cloudfoundry"
 
 describe LogStash::Filters::CloudFoundry do
 
-  describe "Set to Hello World" do
-    let(:config) do <<-CONFIG
-      filter {
-        cloudfoundry {
-        }
-      }
-    CONFIG
-    end
+  subject(:plugin) { LogStash::Filters::CloudFoundry.new(config) }
+  let(:config) { Hash.new }
 
-    sample("message" => "some text") do
-      expect(subject).to include("message")
-      expect(subject['message']).to eq('Hello World')
+  let(:doc)   { "" }
+  let(:event) { LogStash::Event.new("message" => doc) }
+
+  #Received invalid config inputs
+  describe "registration" do
+
+    before(:each) do
+    #  plugin.register
     end
+  
+    context "got blank required data" do
+      let(:config) do
+        {
+			 "cf_api"      => "abc",
+			 "cf_user"     => "asd",
+			 "cf_password" => "water1",
+			 "cf_org"      => "ITCS",
+			 "cf_space"    => "development"}
+      end
+
+      let(:doc) { "sample log" }
+
+      it "should register" do
+        #expect(plugin.cf_api.empty?).to be(false)
+        #expect(plugin.cf_user.empty?).to be(false)
+        #expect(plugin.cf_password.empty?).to be(false)
+        #expect(plugin.cf_org.empty?).to be(false)
+        #expect(plugin.cf_space.empty?).to be(false)
+		expect{subject.register}.to raise_error
+      end
+    end
+	
   end
+  #Received valid config inputs
+  #Check if exception for folder
+  #Check if class variables are set @cf_logged_in, @login_next, @cf_retry_cli
+
+
 end
