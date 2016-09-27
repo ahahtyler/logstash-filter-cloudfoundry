9a8d76c4b1cfbf993304648566ff34ca906bb96a
ahahtyler <tyler.stigliano@verizon.com>
2016-09-26 22:27:17 -0400

    Update cloudfoundry_spec.rb

--- spec/filters/cloudfoundry_spec.rb
+++ spec/filters/cloudfoundry_spec.rb
@@ -14,35 +14,40 @@ describe LogStash::Filters::CloudFoundry do
   describe "registration" do
 
     before(:each) do
-    #  plugin.register
+      plugin.register
     end
-  
-    context "got blank required data" do
-      let(:config) do
-        {
-			 "cf_api"      => "abc",
-			 "cf_user"     => "asd",
-			 "cf_password" => "water1",
-			 "cf_org"      => "ITCS",
-			 "cf_space"    => "development"}
+
+    describe "got blank required data" do
+      let(:config) do {
+            "cf_api"      => "",
+            "cf_user"     => "",
+            "cf_password" => "",
+            "cf_org"      => "ITCS",
+            "cf_space"    => "development"}
       end
 
-      let(:doc) { "sample log" }
-
-      it "should register" do
-        #expect(plugin.cf_api.empty?).to be(false)
-        #expect(plugin.cf_user.empty?).to be(false)
-        #expect(plugin.cf_password.empty?).to be(false)
-        #expect(plugin.cf_org.empty?).to be(false)
-        #expect(plugin.cf_space.empty?).to be(false)
-		expect{subject.register}.to raise_error
+      it "failed variables are set" do
+        expect(plugin.instance_variable_get(:@cf_logged_in)).to eq(false)
+        expect(plugin.instance_variable_get(:@cf_retry_cli)).to eq(false)
       end
     end
-	
+
+    describe "got bad data" do
+      let(:config) do {
+          "cf_api"      => "bad data",
+          "cf_user"     => "bad data",
+          "cf_password" => "bad data",
+          "cf_org"      => "ITCS",
+          "cf_space"    => "development"}
+      end
+
+      it "failed variables are set" do
+        expect(plugin.instance_variable_get(:@cf_logged_in)).to eq(false)
+        expect(plugin.instance_variable_get(:@cf_retry_cli)).to eq(false)
+        expect(File.directory?(plugin.instance_variable_get(:@cf_path))).to be(false)
+      end
+    end
+
   end
-  #Received valid config inputs
-  #Check if exception for folder
-  #Check if class variables are set @cf_logged_in, @login_next, @cf_retry_cli
-
 
 end
